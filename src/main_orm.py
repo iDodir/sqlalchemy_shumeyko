50fc6af308b355f2b820bfa9b126d415199e9f37
dodir <mokronosovmw@gmail.com>
2023-11-01 15:18:01 +0500

    lesson 7: basic async select queries via orm

--- src/main_orm.py
+++ src/main_orm.py
@@ -1,9 +1,28 @@
-from src.queries.orm import SyncORM
+import asyncio
 
-SyncORM.create_tables()
-SyncORM.insert_workers()
-SyncORM.select_workers()
-SyncORM.update_worker()
-SyncORM.select_workers()
-SyncORM.insert_resumes()
-SyncORM.select_resumes_avg_compensation()
+from src.queries.orm import SyncORM, AsyncORM
+
+
+def work_with_sync_orm():
+    SyncORM.create_tables()
+    SyncORM.insert_workers()
+    SyncORM.select_workers()
+    SyncORM.update_worker()
+    SyncORM.select_workers()
+    SyncORM.insert_resumes()
+    SyncORM.select_resumes_avg_compensation()
+
+
+async def work_with_async_orm():
+    await AsyncORM.create_tables()
+    await AsyncORM.insert_workers()
+    await AsyncORM.select_workers()
+    await AsyncORM.update_worker()
+    await AsyncORM.select_workers()
+    await AsyncORM.insert_resumes()
+    await AsyncORM.select_resumes_avg_compensation()
+
+
+if __name__ == "__main__":
+    work_with_sync_orm()
+    asyncio.run(work_with_async_orm())
--- src/queries/orm.py
+++ src/queries/orm.py
@@ -108,4 +108,76 @@ class AsyncORM:
             worker_jack = WorkerOrm(username="Jack")
             worker_michael = WorkerOrm(username="Michael")
             session.add_all([worker_jack, worker_michael])
+            await session.flush()
             await session.commit()
+
+    @staticmethod
+    async def select_workers():
+        async with async_session_factory() as session:
+            query = select(WorkerOrm)
+            result = await session.execute(query)
+            workers = result.scalars().all()
+            print(f"{workers=}")
+
+    @staticmethod
+    async def update_worker(worker_id: int = 2, new_username: str = "Misha"):
+        async with async_session_factory() as session:
+            worker_michael = await session.get(WorkerOrm, worker_id)
+            worker_michael.username = new_username
+            await session.refresh(worker_michael)
+            await session.commit()
+
+    @staticmethod
+    async def insert_resumes():
+        async with async_session_factory() as session:
+            resume_jack_1 = ResumeOrm(
+                title="Python Junior Developer",
+                compensation=50000,
+                workload=Workload.fulltime,
+                worker_id=1,
+            )
+            resume_jack_2 = ResumeOrm(
+                title="Python Разработчик",
+                compensation=150000,
+                workload=Workload.fulltime,
+                worker_id=1,
+            )
+            resume_michael_1 = ResumeOrm(
+                title="Python Data Engineer",
+                compensation=250000,
+                workload=Workload.parttime,
+                worker_id=2,
+            )
+            resume_michael_2 = ResumeOrm(
+                title="Data Scientist",
+                compensation=300000,
+                workload=Workload.fulltime,
+                worker_id=2,
+            )
+            session.add_all(
+                [resume_jack_1, resume_jack_2, resume_michael_1, resume_michael_2],
+            )
+            await session.commit()
+
+    @staticmethod
+    async def select_resumes_avg_compensation(like_language: str = "Python"):
+        async with async_session_factory() as session:
+            query = (
+                select(
+                    ResumeOrm.workload,
+                    # cast(func.avg(ResumeOrm.compensation), Integer).label("avg_compensation"),
+                    func.avg(ResumeOrm.compensation).cast(Integer).label("avg_compensation"),
+                )
+                .select_from(ResumeOrm)
+                .filter(and_(
+                    ResumeOrm.title.contains(like_language),
+                    ResumeOrm.compensation > 40000,
+                ))
+                .group_by(ResumeOrm.workload)
+                .having(func.avg(ResumeOrm.compensation) > 70000)
+            )
+            print(query.compile(compile_kwargs={"literal_binds": True}))
+            res = await session.execute(query)
+            result = res.all()
+            print(result)
+            print(result[0].avg_compensation)
